ef6411d97714a9c2b21c2c2f3b724add2ffc545c
Adrianna (she/her) <61306007+adrivilays@users.noreply.github.com>
2020-10-16 09:49:43 -0700

    Update PictureTester.java

--- PSA5/PictureTester.java
+++ PSA5/PictureTester.java
@@ -1,6 +1,6 @@
 /* 
  * Filename: PictureTester.java
- * Created by: Adrianna Vilaysith A14923721
+ * Created by: Adrianna 
  * Date: May 08th, 2020
  * 
  */
@@ -11,33 +11,33 @@ public class PictureTester
 	
 		/**
 		Picture canvas = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/trees.jpg");
+		("/Documents/bookClass/PSA5/trees.jpg");
 		
 		//canvas.show();
 		
 		Picture copy = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/hibiscus.jpg");
+		("/Documents/bookClass/PSA5/hibiscus.jpg");
 		
 		canvas.placePicture(copy, 200, 200);
 		canvas.explore();
 		**/
 		
 		Picture pic1 = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/collage1.jpg");
+		("/Documents/bookClass/PSA5/collage1.jpg");
 		//pic1.show();
 		
 		Picture pic2 = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/collage2.jpg");
+		("/Documents/bookClass/PSA5/collage2.jpg");
 		//pic2.show();
 		
 		Picture pic3 = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/collage3.jpg");
+		("/Documents/bookClass/PSA5/collage3.jpg");
 		//pic3.show();
 		
 		Picture [] pictures = new Picture[]{pic1, pic2, pic3};
 
 		Picture background = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/background.jpg");
+		("/Documents/bookClass/PSA5/background.jpg");
 		
 		Picture canvas = new Picture(background.getWidth()*3, background.getHeight()*3);
 		
