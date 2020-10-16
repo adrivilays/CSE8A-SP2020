78d14f8806d20ba48844a526ae8b160bd180dd55
Adrianna (she/her) <61306007+adrivilays@users.noreply.github.com>
2020-10-16 09:48:36 -0700

    Update MyCollage.java

--- PSA5/MyCollage.java
+++ PSA5/MyCollage.java
@@ -1,6 +1,6 @@
 /* 
  * Filename: MyCollage.java
- * Created by: Adrianna Vilaysith A14923721
+ * Created by: Adrianna 
  * Date: May 08th, 2020
  * 
  */
@@ -11,15 +11,15 @@ public class MyCollage
 	public static void main(String[] args) {
 		
 		Picture canvas = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/trees.jpg");
+		("/Documents/bookClass/PSA5/trees.jpg");
 		
 		//canvas.show();
 		
 		Picture copy = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/hibiscus.jpg");
+		("/Documents/bookClass/PSA5/hibiscus.jpg");
 		
 		Picture copy2 = new Picture
-		("/Users/adriannavilaysith/Documents/bookClass/PSA5/pineapple.jpg");
+		("/Documents/bookClass/PSA5/pineapple.jpg");
 		
 		canvas.placePicture(copy, 200, 200);
 		canvas.placePicture(copy2, 500, 200); 
